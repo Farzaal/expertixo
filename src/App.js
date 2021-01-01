8c9c97c652c7e7bfe660bfd69944c77305355913
farzal <farzal.khan@grotabyte.com>
2021-01-01 18:30:51 +0500

    question/answer handling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,95 @@
+import React, { useEffect, useState }  from 'react';
 import logo from './logo.svg';
 import './App.css';
+import questions from './questions.js';
+import { Card, Typography, Row, Button, Col, Rate, Progress, Tooltip } from 'antd';
+import "antd/dist/antd.css";
+
+const gridStyle = {
+  width: '50%',
+  textAlign: 'center',
+};
+
+const messageStyle = {
+  width: '100%',
+  textAlign: 'center',
+  marginTop: '40px'
+}
+
+const { Title, Text } = Typography;
 
 function App() {
+  const [current, setCurrent] = useState(0);
+  const [test, setTest] = useState(questions);
+  const [bars, setBard] = useState([
+    {width: 40, color: '#dc3545'},
+    {width: 15, color: '#d5873d'},
+    {width: 15, color: '#f2d261'},
+    {width: 15, color: '#9baa4b'},
+    {width: 15, color: '#6fa053'}
+  ]);
+  const [totalQuestions, setTotalQuestions] = useState(questions.length);
+  const [options, setOptions] = useState([]);
+  const [correct, setCorrect] = useState([]);
+  const [selected, setSelected] = useState(false);
+  const [score, setScore] = useState(0);
+  const [isCorrect, setIsCorrect] = useState(0);
+  const [rating, setRating] = useState(['default','easy','medium','hard'])
+  const [progress, setProgress] = useState(5);
+
+  useEffect(() => {
+    const answer = decodeURI(test[current].correct_answer);
+    const opts = [...test[current].incorrect_answers, answer];
+    setSelected(false);
+    setOptions(opts);
+    setCorrect(answer);
+  }, [current])
+
+  const nextQuestion = () => {
+    setCurrent(current+1)
+    setProgress(progress+5) 
+  }
+
+  const optionSelection = (opt) => {
+    if(opt == correct) {
+      setIsCorrect(true);
+      setScore(score+1);
+    } else
+      setIsCorrect(false);
+
+    setSelected(true);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: '90%', margin: '0 auto', marginTop: '50px', padding: '40px' }}>
+      <Progress percent={progress} />
+      <Card type="inner" title={
+        <div>
+          <Title level={3}>Question {current+1} of {totalQuestions}</Title>
+          <Text type="secondary">{decodeURI(test[current].category.replace(/\?|%3A|%3f|%2C/gi, ''))}</Text><br/>
+          <Rate count={3} defaultValue={rating.indexOf(test[current].difficulty)} />
+        </div>
+      }>
+      <Row>
+        <Title style={{ padding: '20px' }} level={4}>{`${decodeURI(test[current].question.replace(/\?|%3f|%2C/gi, ''))} ?`}</Title>
+      </Row>
+      <Row>
+        {options.map((opt, i) => 
+              <Card.Grid key={i} style={gridStyle} onClick={() => optionSelection(opt)}>{decodeURI(opt)}</Card.Grid>
+        )}
+      </Row>
+      {selected ? 
+      <div>
+        <Row>
+          <Title style={messageStyle} level={3}>{ isCorrect ? 'Correct!' : 'Sorry!' }</Title>
+        </Row>
+        <Row>
+          <Col span={12} offset={11}>
+            <Button onClick={nextQuestion}>Next Question</Button>
+          </Col>
+        </Row>
+      </div> 
+      : ''}
+      </Card>
     </div>
   );
 }
